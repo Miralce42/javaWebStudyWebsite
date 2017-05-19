838dcc62ce9392927b147029c323521bf6c2c0ef
Miralce42 <291176820@qq.com>
2017-05-19 17:26:37 +0800

    增加讨论评论类

---
+++ src/beans/topicComments.java
@@ -0,0 +1,43 @@
+package beans;
+
+/**
+ * Created by 韩壮 on 2017/5/19.
+ */
+public class topicComments {
+    private String topicId;
+    private String commentId;
+    private String username;
+    private String content;
+
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(String commentId) {
+        this.commentId = commentId;
+    }
+}
